ac126975dc19a06e1c430bd3031d913d3cb8df58
guest271314 <guest271314@gmail.com>
2022-11-30 20:57:10 -0800

    Offscreen MediaSession

---
+++ index.js
@@ -0,0 +1,37 @@
+try {
+    const ac = new AudioContext(),
+      mediaElement = new Audio(
+        'https://ia800301.us.archive.org/10/items/DELTAnine2013-12-11.WAV/Deltanine121113Pt3Wav.wav'
+      ),
+      measn = new MediaElementAudioSourceNode(ac, {
+        mediaElement,
+      }),
+      ctx = document.createElement('canvas').getContext('2d'),
+      fontSize = 14,
+      metadata = {
+        title: new URL(mediaElement.src).pathname.split('/').pop(),
+        artist: '',
+        album: '',
+        artwork: [],
+      };
+  
+    ctx.fillStyle = '#ffffff';
+    ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+  
+    const blob = await new Promise((resolve) => ctx.canvas.toBlob(resolve));
+  
+    metadata.artwork.push(
+      {
+        src: URL.createObjectURL(blob),
+        type: 'image/png',
+        sizes: `${ctx.canvas.width}x${ctx.canvas.height}`,
+      },
+    );
+  
+    navigator.mediaSession.metadata = new MediaMetadata(metadata);
+    measn.connect(ac.destination);
+    await mediaElement.play();
+  } catch (err) {
+    console.log(err);
+  }
+  
